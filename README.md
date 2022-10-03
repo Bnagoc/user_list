87d6f9f267d46195cdb42762aec84cd45d03bbb1
Bnag <uladzislau.samuseu@gmail.com>
2022-10-03 22:06:04 +0300

    Spring Boot set up

---
+++ README.md
@@ -0,0 +1,1 @@
+# user_list application\nconsist of: Spring Web, Lombok, PostgreSQL Driver, Spring Data JPA, Thymeleaf
---
+++ src/test/java/com/crud_application/user_list/UserListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_application.user_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
